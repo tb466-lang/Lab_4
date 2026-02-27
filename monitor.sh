27772a4c5a2db654383e8f2e8f99c25feacb2168
Tanish Bhoopathi <tanish.bhoopathi@njit.edu>
2026-02-27 18:03:57 -0500

    Update log header to system report format

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Log the date and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
+echo "SYSTEM REPORT (Memory) - $(date)" >> system_log.txt
 free -h | grep Mem >> system_log.txt
 echo "--------------------------------" >> system_log.txt
